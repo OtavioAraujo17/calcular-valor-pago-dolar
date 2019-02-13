6148fe130a1189fb92fc00a2683da988935eb507
Otavio Araujo <otavio-luiz17@outlook.com>
2019-02-13 00:16:09 -0200

    Projeto valor pago no dolar

---
+++ Valor pago dolar/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+namespace Valor_pago_dolar
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double DolarParaReal(double cotacao, double quantia)
+        {
+            double tot = quantia * cotacao;
+            return tot + tot * Iof / 100.00;
+        }
+ 
+    }
+}
---
+++ Valor pago dolar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Valor_pago_dolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar?");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólar você quer comprar?");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double tot = ConversorDeMoeda.DolarParaReal(cotacao, quantia);
+
+            Console.WriteLine("Valor a ser pago em reais = " + tot.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
